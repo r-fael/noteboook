d0ad3b38048fda6da000c5909fc3a03018e27b29
Rafael Medeiros <rafaelmedeirosfmm@gmail.com>
2025-02-20 15:44:23 -0400

    feat: Add edit book

--- src/components/BookOption/BookOption.tsx
+++ src/components/BookOption/BookOption.tsx
@@ -40,7 +40,6 @@ const PageInput: React.FC<IPageInput> = ({
         autoFocus
         className="bg-zinc-800 text-[0.9rem] p-1 ml-8 outline-0 rounded-sm border-3 border-zinc-700 font-black"
         onKeyDown={(e) => {
-          console.log(e.currentTarget.value);
           if (e.key === 'Enter') {
             if (e.currentTarget.value.trim() != '') {
               handleValue(e.currentTarget.value);
@@ -50,7 +49,6 @@ const PageInput: React.FC<IPageInput> = ({
           }
         }}
         onBlur={(e) => {
-          console.log(e.target.value);
           if (e.target.value.trim() !== '') {
             handleValue(e.target.value.trim());
           }
@@ -68,8 +66,21 @@ const PageInput: React.FC<IPageInput> = ({
 
 const BookOption: React.FC<IBookOption> = ({ book }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
   const [pageInputIsOpen, setPageInputIsOpen] = useState<boolean>(false);
-  const { selectedBook } = useBooks();
+  const [error, setError] = useState<boolean>(false);
+
+  const { selectedBook, books, editBook } = useBooks();
+
+  const handleValue = (value: string) => {
+    if (books.map((book) => book.name).includes(value)) {
+      setError(true);
+    } else {
+      setError(false);
+      editBook(book.name, value);
+      setIsEditing(false);
+    }
+  };
 
   return (
     <div className="flex flex-col select-none font-black">
@@ -79,8 +90,46 @@ const BookOption: React.FC<IBookOption> = ({ book }) => {
         } flex flex-row gap-2 items-center rounded-sm text-[1rem] hover:bg-zinc-800 p-2  hover:cursor-pointer`}
         onClick={() => setIsOpen(!isOpen)}
       >
-        {isOpen ? <Down /> : <Right />}
-        <h3>{book.name}</h3>
+        <div className="w-6 h-6 flex items-center justify-center">
+          {isOpen ? <Down /> : <Right />}
+        </div>
+
+        {isEditing ? (
+          <div className="flex flex-col gap-2">
+            <input
+              autoFocus
+              defaultValue={book.name}
+              className="text-[0.9rem] outline-0 w-[8rem]"
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  if (e.currentTarget.value.trim() != '') {
+                    handleValue(e.currentTarget.value);
+                  } else {
+                    setError(false);
+                    setIsEditing(false);
+                  }
+                }
+              }}
+              onBlur={(e) => {
+                if (e.target.value.trim() !== '') {
+                  handleValue(e.target.value.trim());
+                }
+                setError(false);
+                setIsEditing(false);
+              }}
+            />
+          </div>
+        ) : (
+          <h3
+            className="break-all"
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsEditing(true);
+            }}
+          >
+            {book.name}
+          </h3>
+        )}
         {book.pages.length > 0 ? (
           <h1 className="bg-zinc-700 text-[0.8rem] font-black rounded-4xl px-[0.5rem] h-[1.2rem] flex items-center justify-center">
             {book.pages.length}
@@ -106,6 +155,11 @@ const BookOption: React.FC<IBookOption> = ({ book }) => {
             + add page
           </div>
         </div>
+      ) : null}
+      {error ? (
+        <span className="text-[0.8rem] text-red-400  p-1 ml-8 mt-2">
+          Error: this page name was already added
+        </span>
       ) : null}
     </div>
   );
