d8ad26779db60a943e6b5f2a844a7053934cb6a9
Rafael Medeiros <rafaelmedeirosfmm@gmail.com>
2025-02-20 21:50:19 -0400

    feat: Update style of book list

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -30,10 +30,10 @@ const BookInput: React.FC<IBookInput> = ({
   };
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 text-[1rem]">
       <input
         autoFocus
-        className="bg-zinc-800 self-center p-2 outline-0 rounded-sm border-4 border-zinc-700 font-black"
+        className="bg-zinc-800 p-2 outline-0 rounded-sm border-4 border-zinc-700 font-black"
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             if (e.currentTarget.value.trim() != '') {
@@ -64,7 +64,7 @@ const BooksList: React.FC<IBooksList> = () => {
   const [bookInputIsOpen, setBookInputIsOpen] = useState<boolean>(false);
 
   return (
-    <div className="text-xs lg:w-[15vw] w-auto flex flex-col p-2 gap-2">
+    <div className="text-xs lg:w-[15vw] w-auto flex flex-col p-2 gap-2 lg:h-[75vh] h-[40rem] mt-[3rem] overflow-y-scroll">
       {books.map((book, key) => (
         <BookOption key={key} book={book} />
       ))}
@@ -75,7 +75,7 @@ const BooksList: React.FC<IBooksList> = () => {
         />
       ) : null}
       <div
-        className="flex font-black rounded-sm text-[1.1rem] p-2 hover:cursor-pointer justify-start hover:border-zinc-100 border-2 my-4 border-zinc-950 text-zinc-500 hover:text-zinc-100 transition ease-in"
+        className="flex font-black rounded-sm text-[1.1rem] p-2 hover:cursor-pointer justify-start hover:border-zinc-100 border-2 mb-4 border-zinc-950 text-zinc-500 hover:text-zinc-100 transition ease-in"
         onClick={() => setBookInputIsOpen(true)}
       >
         + add book
